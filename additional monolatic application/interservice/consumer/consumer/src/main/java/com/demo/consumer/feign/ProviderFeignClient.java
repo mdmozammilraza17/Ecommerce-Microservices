b614f5d0c083b4fed1978970675b25922a7242c5
mdmozammilraza17 <mdmozammilraza06@gmail.com>
2025-10-23 19:25:51 +0530

    Eureka server practice

--- additional monolatic application/interservice/consumer/consumer/src/main/java/com/demo/consumer/feign/ProviderFeignClient.java
+++ additional monolatic application/interservice/consumer/consumer/src/main/java/com/demo/consumer/feign/ProviderFeignClient.java
@@ -3,7 +3,8 @@ package com.demo.consumer.feign;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
-@org.springframework.cloud.openfeign.FeignClient(name="provider-service", url="http://localhost:8081")
+//@org.springframework.cloud.openfeign.FeignClient(name="provider-service", url="http://localhost:8081")
+@org.springframework.cloud.openfeign.FeignClient(name="provider")
 public interface ProviderFeignClient {
 
     @GetMapping("/instance-info")
