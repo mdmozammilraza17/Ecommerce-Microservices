9686b1818161258341242333ddbfbe41e369ddd3
mdmozammilraza17 <mdmozammilraza06@gmail.com>
2025-11-19 13:08:57 +0530

    Integrated RabbitMQ with Spring Boot

--- gateway/gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
+++ gateway/gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
@@ -1,13 +1,30 @@
 package com.ecommerce.gateway;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GatewayConfig {
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter ()
+    {
+        return new RedisRateLimiter(1, 1, 1);
+    }
+
+    @Bean
+    public KeyResolver hostNameKeyResolver ()
+    {
+        return exchange -> Mono.just(
+                exchange.getRequest().getRemoteAddress().getHostName());
+    }
+
 
     @Bean
     public RouteLocator customRouteLocator (RouteLocatorBuilder builder)
@@ -19,6 +36,9 @@ public class GatewayConfig {
                                 f.retry(retryConfig -> retryConfig
                                                 .setRetries(10)
                                                 .setMethods(HttpMethod.GET))
+                                        .requestRateLimiter(config -> config
+                                                .setRateLimiter(redisRateLimiter())
+                                                .setKeyResolver(hostNameKeyResolver()))
                                 .circuitBreaker(config -> config
                                 .setName("ecomBreaker")
                                 .setFallbackUri("forward:/fallback/products")))
