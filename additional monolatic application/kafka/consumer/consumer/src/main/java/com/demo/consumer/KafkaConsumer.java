90759b840da22ac56478c15f0c32d370cd6d4147
mdmozammilraza17 <mdmozammilraza06@gmail.com>
2025-11-23 17:01:56 +0530

    Setting up Consumer Groups

--- additional monolatic application/kafka/consumer/consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
+++ additional monolatic application/kafka/consumer/consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
@@ -7,8 +7,14 @@ import org.springframework.stereotype.Component;
 public class KafkaConsumer {
 
     @KafkaListener(topics = "my-topic", groupId = "my-new-group")
-    public void listen(String message)
+    public void listen1(String message)
     {
-        System.out.println("Received Message: " + message);
+        System.out.println("Received Message 1: " + message);
+    }
+
+    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
+    public void listen2(String message)
+    {
+        System.out.println("Received Message 2: " + message);
     }
 }
